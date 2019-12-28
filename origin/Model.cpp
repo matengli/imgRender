ee31b4fc31df58a3d562e63dc3e0350c047f6105
matengli <matengli@elestorm.com>
2019-12-29 03:46:27 +0800

    fix split for  better perfomance

--- origin/Model.cpp
+++ origin/Model.cpp
@@ -14,9 +14,25 @@ bool Model::readFromFile(const char *fileName) {
         std::cerr << "can't open file " << fileName << "\n";
         return false;
     }
-    while (std::getline(in, line)) {
-        auto strVec = splice(line, " ");
-        this->handleOutputData(strVec);
+    while (!in.eof()) {
+        std::getline(in, line);
+        std::stringstream str(line);
+        char trash;
+        if(!line.compare(0,2,"v ")){
+            float x,y,z;
+            str>>trash>>x>>y>>z;
+            points.push_back(vec4f(x,y,z,0.));
+        } else if (!line.compare(0,2,"f ")){
+            str>>trash;
+
+            std::vector<int> val;
+            int ival, itrash;
+            char ctrash;
+            while (str>>ival>>ctrash>>itrash>>ctrash>>itrash) {
+                val.push_back(ival-1);
+            }
+            faces.push_back(val);
+        }
     }
 }
 
@@ -29,17 +45,21 @@ Model::Model(const char *fileName) {
 }
 
 bool Model::handleOutputData(std::vector<std::string> strVec) {
-    if (strVec[0] == "v") {
-        this->points.push_back(vec4f(str2float(strVec[1]), str2float(strVec[2]), str2float(strVec[3]), 0.));
-    } else if (strVec[0] == "f") {
-
-        std::vector<int> val;
-
-        for (int i = 0; i < 3; i++) {
-            auto strArg = splice(strVec[i + 1], "/");
-            val.push_back(str2int(strArg[0]) - 1);
-        }
-        this->faces.push_back(val);
-    }
-    return false;
+//    if (strVec[0] == "v") {
+//        this->points.push_back(vec4f(str2float(strVec[1]), str2float(strVec[2]), str2float(strVec[3]), 0.));
+//    } else if (strVec[0] == "f") {
+//
+//        std::vector<int> val;
+//
+//        for (int i = 0; i < 3; i++) {
+//            std::istringstream iss(strVec[i+1]);
+//            int ival, itrash;
+//            char ctrash;
+//            while (iss>>ival>>ctrash>>itrash>>ctrash>>itrash) {
+//                val.push_back(ival-1);
+//            }
+//        }
+//        this->faces.push_back(val);
+//    }
+//    return false;
 }
